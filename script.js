c90c892b74d17c7e9cf3e9a584c9340ec5d1c791
Kgaspic <katarina.gaspic@gmail.com>
2023-03-10 21:20:17 +0100

    jsupdate

--- script.js
+++ script.js
@@ -25,21 +25,19 @@ drone.on("open", (error) => {
   });
   room.on("message", (message) => {
     console.log(message);
-  
+
     let messageDiv = document.createElement("div");
-    const userColor = generateColor(message.member.clientData.name); 
+    const userColor = generateColor(message.member.clientData.name);
     const userName = message.member.clientData.name;
-  
+
     messageDiv.innerHTML = `<div class="username" style="color:${userColor}">${userName}</div><div class="message">${message.data}</div>`;
     if (myUsername === userName) {
       messageDiv.style.textAlign = "right";
     }
-  
+
     messageList.appendChild(messageDiv);
   });
-  
-  });
-
+});
 
 messageSend.addEventListener("click", () => {
   const inputValue = messageInput.value;
@@ -64,71 +62,70 @@ messageInput.addEventListener("keyup", (event) => {
 });
 
 function generateColor(string) {
-  var hash = 0;
-  for (var i = 0; i < string.length; i++) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  var colour = "#";
-  for (var i = 0; i < 3; i++) {
-    var value = (hash >> (i * 8)) & 0xff;
-    colour += ("00" + value.toString(16)).substr(-2);
-  }
-  return colour;
+  const hash = string.split("").reduce((acc, char) => {
+    return char.charCodeAt(0) + ((acc << 5) - acc);
+  }, 0);
+
+  const colour = Array.from({ length: 3 }, (_, index) => {
+    const value = (hash >> (index * 8)) & 0xff;
+    return ("00" + value.toString(16)).substr(-2);
+  }).join("");
+
+  return `#${colour}`;
 }
 
 function generateName() {
   const names = [
     "Ivo",
-"Mia",
-"Luka",
-"Tina",
-"Marko",
-"Ela",
-"Ivan",
-"Mara",
-"Nikola",
-"Nina",
-"Ante",
-"Marta",
-"Stjepan",
-"Ana",
-"Petar",
-"Eva",
-"Josip",
-"Lea",
-"Mirko",
-"Dora",
-"Toni",
-"Sara",
-"Leo",
-"Klara",
-"Matija",
-"Dina",
-"Karlo",
-"Ivana",
-"Filip",
-"Maja",
-"Lovro",
-"Lucija",
-"Juraj",
-"Iris",
-"Mario",
-"Lena",
-"Borna",
-"Tea",
-"Šime",
-"Marta",
-"Patrik",
-"Ivana",
-"Bruno",
-"Mila",
-"Stipe",
-"Ena",
-"Marin",
-"Jana",
-"Andro",
-"Ines"
+    "Mia",
+    "Luka",
+    "Tina",
+    "Marko",
+    "Ela",
+    "Ivan",
+    "Mara",
+    "Nikola",
+    "Nina",
+    "Ante",
+    "Marta",
+    "Stjepan",
+    "Ana",
+    "Petar",
+    "Eva",
+    "Josip",
+    "Lea",
+    "Mirko",
+    "Dora",
+    "Toni",
+    "Sara",
+    "Leo",
+    "Klara",
+    "Matija",
+    "Dina",
+    "Karlo",
+    "Ivana",
+    "Filip",
+    "Maja",
+    "Antun",
+    "Lucija",
+    "Juraj",
+    "Iris",
+    "Mario",
+    "Lena",
+    "Borna",
+    "Tea",
+    "Šime",
+    "Marta",
+    "Patrik",
+    "Ivana",
+    "Bruno",
+    "Katarina",
+    "Stipe",
+    "Ena",
+    "Marin",
+    "Jana",
+    "Andro",
+    "Ines",
   ];
   return names[Math.floor(Math.random() * names.length)];
 }
-
